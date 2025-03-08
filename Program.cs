1c180ef2e545f3b47d5367e31a5738b55b90e73a
MohsinJutt62 <mohsinbatth281@gmail.com>
2025-03-08 17:54:15 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,33 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace Lab_3___Q2
+namespace Lab_3__Q3
 {
-    class Fraction
+    class MealCard
     {
-        //Q # 2
-        public
-        double Dividend; double Divisor;
-        public Fraction(double dividend, double divisor)
+        //Q # 3
+        public int balance;
+        public string studentName;
+        public MealCard(string name, int initialBalance = 100)
         {
-            if (dividend == 0) { Console.WriteLine($"Error: Dividend cant be 0 ...........");
-                dividend = 1;
-            }
-
-            Dividend=dividend;  Divisor = divisor;
-        } 
-        public void Display()
-        {
-            Console.WriteLine($"Fraction : {Dividend} / {Divisor}");
+            studentName = name;
+            balance = initialBalance;
         }
-
+        public void AddPoints(int points)
+        {
+            if (points > 0)
+            {
+                balance += points;
+                Console.WriteLine($"{points} points added. New balance: {balance}.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid amount. Please enter a positive number.");
+            }
+        }
+        public void PurchaseFood(int points)
+        {
+            if (points > 0 && balance >= points)
+            {
+                balance -= points;
+                Console.WriteLine($"Food purchased for {points} points. Remaining balance: {balance}.");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient balance or invalid amount. Purchase denied.");
+            }
+        }
+        public void CheckBalance()
+        {
+            Console.WriteLine($"{studentName}'s Meal Card Balance: {balance} points.");
+        }
+    }
+    class Mealcard
+    {
         static void Main(string[] args)
         {
-            Fraction fraction1 = new Fraction(12,2);
-            fraction1.Display();
-            Fraction fraction2 = new Fraction(34, 4);
-            fraction2.Display();
+            MealCard student1 = new MealCard("Ali");
+            student1.CheckBalance();
+            student1.PurchaseFood(30);
+            student1.CheckBalance();
+            student1.AddPoints(50);
+            student1.CheckBalance();
+            student1.PurchaseFood(150); 
         }
     }
 }
